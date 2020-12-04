80df5c25ab2229148c81cc6cbc1859cc18f850b8
Aniko A. Liptak <anikoa.liptak@gmail.com>
2020-12-04 18:20:40 +0100

    returned source path

---
+++ get_home/app.js
@@ -0,0 +1,11 @@
+console.log('A számológép használatra kész.');
+
+// Which I have to cakcukate
+// fuelToOffload:
+    // The fuel to be drained
+    // fuelToOffload = tankCapacity - requiredFuel
+// tankCapacity: fuel tank capacity 
+// requiredFuel: the minimun amount of fuel required
+
+
+
